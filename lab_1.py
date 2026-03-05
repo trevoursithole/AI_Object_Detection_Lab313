9f5120db7bb7a1bb5498457e6210dad59209c1ab
Trevour <trevourthetutor@gmail.com>
2026-03-05 23:19:05 +0200

    Final Lab 1 submission with results and screenshots

--- lab_1.py
+++ lab_1.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")  # downloads model on first run
 
-source ="World_Class_Traffic_Jam_2_(7454479488).JPG"  # change to image, video, or 0 for webcam
+source ="Asian market.jpg"  # change to image, video, or 0 for webcam
 
 if isinstance(source, str) and source.lower().endswith((".jpg", ".jpeg", ".png", ".bmp")):
     model.predict(source=source, conf=0.35, show=True, save=True)
