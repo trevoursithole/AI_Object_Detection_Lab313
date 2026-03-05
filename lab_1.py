16fc7f12abe2d9268813f1e2b9da43e9fa4f2baf
Trevour <trevourthetutor@gmail.com>
2026-03-05 22:37:12 +0200

    Final Lab 1 submission with results and screenshots

--- lab_1.py
+++ lab_1.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")  # downloads model on first run
 
-source = r"Cars Moving On Road Footage.mp4"  # change to image, video, or 0 for webcam
+source ="World_Class_Traffic_Jam_2_(7454479488).JPG"  # change to image, video, or 0 for webcam
 
-if source.lower().endswith((".jpg", ".jpeg", ".png", ".bmp")):
+if isinstance(source, str) and source.lower().endswith((".jpg", ".jpeg", ".png", ".bmp")):
     model.predict(source=source, conf=0.35, show=True, save=True)
 else:
     model.track(source=source, tracker="bytetrack.yaml", conf=0.35, show=True, save=True)
